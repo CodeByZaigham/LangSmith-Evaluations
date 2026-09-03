6a21941557eb2bcfabdd98cb0a1bd2750834b9f3
Syed Zaigham Hussain Shah <zaigham.hussain@hyperdata.com>
2026-09-03 13:33:32 +0500

    Multi query retrieving strategy configured using similarity search

--- tracings/RAG_appication/retriever.py
+++ tracings/RAG_appication/retriever.py
@@ -1,3 +1,20 @@
+from langchain_community.vectorstores import FAISS
 from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+from langsmith import traceable
 from LLM import get_LLM
 
+@traceable(name="retrieving chunks from vector store")
+def retrieve_embeddings(query:str , db:FAISS):
+        retriever=db.as_retriever(
+            search_type="similarity",
+            search_kwargs={"k":4}
+        )
+
+        query_variations=MultiQueryRetriever.from_llm(
+            llm=get_LLM(),
+            retriever=retriever
+        )
+
+        result=query_variations.invoke(query)
+
+        return result
