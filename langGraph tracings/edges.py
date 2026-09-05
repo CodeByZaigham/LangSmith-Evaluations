f643b89a6d08235d517ec733860d557f1ca09c68
CodeByZaigham <zaigham.shah2004@gmail.com>
2026-09-05 22:10:45 +0500

    edges added

--- langGraph tracings/edges.py
+++ langGraph tracings/edges.py
@@ -1,4 +1,25 @@
 from langgraph.graph import START,END,StateGraph
+from nodes import (
+     evaluate_analysis,
+     evaluate_language,
+     evaluate_thought,
+     final_evaluation
+)
 from state import state
 
 graph=StateGraph(state)
+
+graph.add_node("evaluate_language", evaluate_language)
+graph.add_node("evaluate_analysis", evaluate_analysis)
+graph.add_node("evaluate_thought", evaluate_thought)
+graph.add_node("final_evaluation", final_evaluation)
+
+graph.add_edge(START, "evaluate_language")
+graph.add_edge(START, "evaluate_analysis")
+graph.add_edge(START, "evaluate_thought")
+graph.add_edge("evaluate_language", "final_evaluation")
+graph.add_edge("evaluate_analysis", "final_evaluation")
+graph.add_edge("evaluate_thought", "final_evaluation")
+graph.add_edge("final_evaluation", END)
+
+workflow = graph.compile()
